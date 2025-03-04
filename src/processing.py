f6ccf2367334b48f94199c474d82b89d6f98acd4
Egozzar <Egozzar@yandex.ru>
2025-03-04 19:40:56 +0300

    function sort_subfunc added to the processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from typing import Any
+
 type Oper = list[dict[str, int | str]]
 
 
@@ -12,3 +15,15 @@ def filter_by_state(operations: Oper, state: str = "EXECUTED") -> Oper:
     selected_operations = filter(lambda x: x["state"] == state, operations)
 
     return list(selected_operations)
+
+
+def sort_subfunc(operation: dict[str, Any]) -> datetime:
+    """
+     Вспомогательная функция получения ключа (key) для сортировки.
+     Принимает словарь с данными по банковской операции и возвращает объект даты проведения этой операции.
+    :param operation: (dict[str, int | str]) словарь с данными по банковской операции
+    :return: (datetime) объект даты операции
+    """
+    moment = datetime.strptime(operation["date"], "%Y-%m-%dT%H:%M:%S.%f")
+
+    return moment
