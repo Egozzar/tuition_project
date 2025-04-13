ccb0e16869c82022df91c913ccfbc334d68d231d
Egozzar <Egozzar@yandex.ru>
2025-04-13 13:25:58 +0300

    fixes utils.py, test_utils.py, conftest.py

--- src/utils.py
+++ src/utils.py
@@ -23,13 +23,8 @@ def loader_json(address: str = way) -> list:
     :return:(list) список словарей с данными о финансовых транзакциях
     """
     try:
-        with open(address, encoding="UTF-8") as file:
-            content = file.read()
-            if not content:
-                logger.info("В полученном файле нет содержимого. Возвращён пустой список.")
-                return []
-
-            result = json.loads(content)
+        with open(address, encoding="UTF=8") as file:
+            result = json.load(file)
             if isinstance(result, list):
                 logger.info("Содержимое полученного файла успешно преобразовано в список словарей")
                 return result
@@ -40,16 +35,3 @@ def loader_json(address: str = way) -> list:
     except FileNotFoundError as err:
         logger.error(f"{err}")
         return []
-
-
-#
-#
-# if __name__ == "__main__":
-#     way2 = os.path.join(ROOT_PATH, "tests", "ex2.json")
-#     way3 = os.path.join(ROOT_PATH, "tests", "ex3.json")
-#     way4 = os.path.join(ROOT_PATH, "tests", "ex.json")
-#
-#     loader_json()
-#     loader_json(way2)
-#     loader_json(way3)
-#     loader_json(way4)
