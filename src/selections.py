c7c38e4b8262774c10ed89c380b43b60d7b8c214
Egozzar <Egozzar@yandex.ru>
2025-04-14 21:56:32 +0300

    added selections.py with function selection_by_exp & test_selections.py

---
+++ src/selections.py
@@ -0,0 +1,16 @@
+import re
+from typing import Any
+
+
+def selection_by_exp(operations: list[dict | Any], expression: str = "") -> list[dict | Any]:
+    """
+    Функция для фильтрации списка словарей операций по заданной строке,
+    возвращает отфильтрованный список транзакций
+    :param operations:(list[dict | Any]) список словарей транзакций
+    :param expression:(str) строка в описании, по которой фильтруется список транзакций
+    :return:(list[dict | Any]) отфильтрованный список транзакций
+    """
+    pattern = re.compile(f"{expression.lower()}")
+    lst = [elem for elem in operations if re.search(pattern, elem.get("description", "").lower())]
+
+    return lst
